f1093b2000da3ea4ad76edaf79e06f03632600b6
Matt <85322+mattmassicotte@users.noreply.github.com>
2024-03-03 10:16:18 -0500

    Raising minimum to match requirements

--- Package.swift
+++ Package.swift
@@ -6,10 +6,10 @@ let package = Package(
 	name: "Background",
 	platforms: [
 		.macOS(.v11),
-		.iOS(.v13),
-		.tvOS(.v13),
-		.watchOS(.v6),
-		.macCatalyst(.v13),
+		.iOS(.v14),
+		.tvOS(.v14),
+		.watchOS(.v7),
+		.macCatalyst(.v14),
 	],
 	products: [
 		.library(name: "Background", targets: ["Background"]),
