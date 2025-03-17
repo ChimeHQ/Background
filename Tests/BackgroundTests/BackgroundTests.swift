ea4d5a773b9ab680764677ab065adfa28ef638ca
Matt <85322+mattmassicotte@users.noreply.github.com>
2025-03-17 10:53:58 -0400

    expect throws during non-macOS background tests

--- Tests/BackgroundTests/BackgroundTests.swift
+++ Tests/BackgroundTests/BackgroundTests.swift
@@ -3,16 +3,24 @@ import Testing
 
 #if os(iOS) || os(tvOS) || os(visionOS) || os(macOS)
 struct BackgroundTests {
-	@Test func testExample() throws {
-		let request = ProcessingTaskRequest(identifier: "abc")
+	@Test func registerAndSubmitProcessingTask() throws {
+		let identifier = "abc"
+		let request = ProcessingTaskRequest(identifier: identifier)
 		
 		let scheduler = TaskScheduler.shared
 		
-		_ = scheduler.register(forTaskWithIdentifier: "abc") { task in
+		_ = scheduler.register(forTaskWithIdentifier: identifier) { task in
 			
 		}
 		
+#if os(macOS)
 		try scheduler.submit(request)
+#else
+		// we know this will throw because we will not have the correct plist entries during testing
+		#expect(throws: (any Error).self) {
+			try scheduler.submit(request)
+		}
+#endif
 	}
 }
 #endif
